7a68313dd93e8c5292f737a5bdcf730af73e9338
Jesus Marron <jesusmarron7@gmail.com>
2023-07-07 11:07:10 -0500

    createPrompt implemented. Tests passing

--- src/lib.js
+++ src/lib.js
@@ -19,8 +19,29 @@ export const chooseRandom = (array = [], numItems) => {
   return questions.slice(0, numItems);
 }
 
-export const createPrompt = (object) => {
-  // TODO implement createPrompt
+export const createPrompt = ({ numQuestions = 1, numChoices = 2 } = {}) => {
+  let result = [];
+
+  for (let q = 1; q <= numQuestions; q++) {
+    result.push(
+      {
+        type: 'input',
+        name: `question-${q}`,
+        message: `Enter question ${q}`
+      }
+    );
+
+    for (let c = 1; c <= numChoices; c++) {
+      result.push(
+        {
+          type: 'input',
+          name: `question-${q}-choice-${c}`,
+          message: `Enter answer choice ${c} for question ${q}`
+        }
+      );
+    }
+  }
+  return result;
 }
 
 export const createQuestions = () => {
